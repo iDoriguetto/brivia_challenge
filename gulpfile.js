e894ba7552772190aee070d73a1a31e5dd6b6a9b
Isabella Doriguetto <dorighetta007@gmail.com>
2021-11-10 18:27:58 -0300

    gulp 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require
+const { src, dest }  = require("gulp");
+const minify = require("gulp-minify");
+
+function minifyjs() {
+
+    return src('scss/style.js', { allowEmpty: true })
+        .pipe(minify({noSource: true}))
+}
+
+exports.default = minifyjs;
